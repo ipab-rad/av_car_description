92287388a819103d94addf28f7808d77b38435c3
Alejandro Bordallo <alex.bordallo@gmail.com>
2024-04-26 13:14:05 +0100

    Fix pre-commit linting, advice and suggestions

--- runtime.sh
+++ runtime.sh
@@ -6,17 +6,17 @@
 # Initialise CMD as empty
 CMD=""
 
-# If an arg is defined, start contaier with bash
+# If an arg is defined, start container with bash
 if [ -n "$1" ]; then
     CMD="bash"
 fi
 
 # Build docker image only up to base stage
 DOCKER_BUILDKIT=1 docker build \
--t av_car_description:latest \
--f Dockerfile --target runtime .
+    -t av_car_description:latest \
+    -f Dockerfile --target runtime .
 
 # Run docker image without volumes
 docker run -it --rm --net host \
--v /dev/shm:/dev/shm \
-av_car_description:latest $CMD
+    -v /dev/shm:/dev/shm \
+    av_car_description:latest $CMD
