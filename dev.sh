fb43769dcfbc7a17fe5da052374dd18e97aacffc
Hector Cruz <hcruzgo@ed.ac.uk>
2024-04-30 11:38:52 +0100

    Fixing scripts and README

--- dev.sh
+++ dev.sh
@@ -11,6 +11,6 @@ DOCKER_BUILDKIT=1 docker build \
 # Run docker image with local code volumes for development
 docker run -it --rm --net host \
     -v /dev/shm:/dev/shm \
-    -v ./car_description:/opt/ros_ws/src/car_description \
-    -v ./car_meshes:/opt/ros_ws/src/car_meshes \
+    -v ./av_car_description:/opt/ros_ws/src/car_description \
+    -v ./av_car_meshes:/opt/ros_ws/src/av_car_meshes \
     av_car_description:latest
