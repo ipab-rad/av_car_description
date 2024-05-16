c4a9f0261775738b66cdf0728f0254eaa6661a0f
hect95 <hector_cruz95@live.com>
2024-05-16 13:01:47 +0100

    Avoid override dev and runtime images

--- dev.sh
+++ dev.sh
@@ -5,7 +5,7 @@
 
 # Build docker image up to dev stage
 DOCKER_BUILDKIT=1 docker build \
-    -t av_car_description:latest \
+    -t av_car_description:latest-dev \
     -f Dockerfile --target dev .
 
 # Run docker image with local code volumes for development
@@ -13,4 +13,4 @@ docker run -it --rm --net host \
     -v /dev/shm:/dev/shm \
     -v ./av_car_description:/opt/ros_ws/src/car_description \
     -v ./av_car_meshes:/opt/ros_ws/src/av_car_meshes \
-    av_car_description:latest
+    av_car_description:latest-dev
