b4c4f68bb00d4fa1e656301622e8d5577203bc4e
hect95 <hector_cruz95@live.com>
2024-05-20 16:12:21 +0100

    Synchronise host time with container

--- dev.sh
+++ dev.sh
@@ -11,6 +11,7 @@ DOCKER_BUILDKIT=1 docker build \
 # Run docker image with local code volumes for development
 docker run -it --rm --net host \
     -v /dev/shm:/dev/shm \
+    -v /etc/localtime:/etc/localtime:ro \
     -v ./av_car_description:/opt/ros_ws/src/car_description \
     -v ./av_car_meshes:/opt/ros_ws/src/av_car_meshes \
     av_car_description:latest-dev
